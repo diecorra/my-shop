ff2425b487428e7a45d7745c90fe438362d21de1
Diego Corradi <diego.corradi95@gmail.com>
2023-06-20 10:25:33 +0200

    update ThanksPage

--- src/features/checkout/ThanksPage.tsx
+++ src/features/checkout/ThanksPage.tsx
@@ -1,5 +1,15 @@
-export const ThanksPage = () => {
-  return <div>ThanksPage</div>;
-};
+import { NavLink } from 'react-router-dom';
 
-export default ThanksPage;
+export function ThanksPage() {
+  return (
+    <div>
+      <div className="text-3xl text-center">Thank you your order</div>
+
+      <div className="flex justify-center mt-12">
+        <NavLink to="/shop" className="btn primary">
+          Back to Shop
+        </NavLink>
+      </div>
+    </div>
+  );
+}
